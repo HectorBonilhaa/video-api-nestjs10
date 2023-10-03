255dbb8b097758da8f4819eae3212e3a16b1fac2
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-10-03 19:40:36 -0300

    ending project

---
+++ src/videos/videos.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@nestjs/common';
+import { CreateVideoDto } from './dto/create-video.dto';
+import { UpdateVideoDto } from './dto/update-video.dto';
+import { PrismaService } from '../prisma/prisma/prisma.service';
+import { InvalidRelationError } from '../errors/invalid-relation.error';
+
+@Injectable()
+export class VideosService {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(createVideoDto: CreateVideoDto & { file: Express.Multer.File }) {
+    const categoryExists =
+      (await this.prismaService.category.count({
+        where: {
+          id: createVideoDto.category_id,
+        },
+      })) != 0;
+
+    if (!categoryExists) {
+      throw new InvalidRelationError('Category Not Found!');
+    }
+
+    const createdVideo = this.prismaService.video.create({
+      data: {
+        title: createVideoDto.title,
+        description: createVideoDto.description,
+        category_id: createVideoDto.category_id,
+        file_path: createVideoDto.file.path,
+      },
+    });
+
+    return createdVideo;
+  }
+
+  findAll() {
+    return this.prismaService.video.findMany({
+      include: {
+        category: true,
+      },
+    });
+  }
+
+  findOne(id: number) {
+    return this.prismaService.video.findFirstOrThrow({
+      where: {
+        id,
+      },
+    });
+  }
+
+  update(id: number, updateVideoDto: UpdateVideoDto) {
+    return `This action updates a #${id} video`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} video`;
+  }
+}
